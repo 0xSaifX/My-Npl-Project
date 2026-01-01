4ab50c5bf38df0fbdac0c8b15186207b3c01aa2b
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-01 13:29:46 +0100

    auto-commit

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -460,7 +460,7 @@ function PartnersSection() {
           </div>
         </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 w-full">
             {partners.map((partner, index) => (
               <div key={index} className="flex flex-col gap-6 ">
                 <div className="flex h-[300px] justify-center items-center rounded-2xl" style={{ backgroundColor: partner.bg }}>
