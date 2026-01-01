ffd85b51a49f9c38fbf597d4e64052a2858227eb
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-01 06:24:52 +0100

    auto-commit

--- src/pages/FourthPage.jsx
+++ src/pages/FourthPage.jsx
@@ -25,7 +25,7 @@ export default function FourthPage() {
         <div className="max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-0 py-12">
 
           {/*  INTRO  */}
-          <section>
+          <section className="m-20">
             <h2 className="text-lg font-semibold">NPL 특수물건 학원</h2>
             <p className="mt-2 text-sm text-gray-500">
               NPL 전문가가 되고 싶으신가요? 차별화된 NPL 교육을 직접 경험해보세요.
