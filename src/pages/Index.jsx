e5a61e3aae7e62fb06ac379e3ac2c2a704a0f0be
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-01 15:26:31 +0100

    auto-commit

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -826,24 +826,7 @@ const partnerIcons = [
               전국에 있는 풀하우스 제휴업체를 만나보세요.
             </div>
           </div>
-
-          <div className="flex flex-col items-center gap-7">
-            <div className="flex flex-col items-start w-[680px]">
-              <div className="flex flex-col items-start gap-1.5 w-full">
-                <div className="flex h-16 max-h-16 px-7 py-4.5 items-center w-full rounded-full bg-gray-200">
-                  <div className="flex-1 text-lg font-pretendard text-gray-500">검색어를 입력하세요.</div>
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M10 4C6.68629 4 4 6.68629 4 10C4 13.3137 6.68629 16 10 16C13.3137 16 16 13.3137 16 10C16 6.68629 13.3137 4 10 4ZM2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 11.8487 17.3729 13.5509 16.3199 14.9056L21.7071 20.2929C22.0976 20.6834 22.0976 21.3166 21.7071 21.7071C21.3166 22.0976 20.6834 22.0976 20.2929 21.7071L14.9057 16.3198C13.551 17.3729 11.8487 18 10 18C5.58172 18 2 14.4183 2 10Z"
-                      fill="#616161"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-            {/* ICON SLIDER */}
+          {/* ICON SLIDER */}
 <div className="w-full overflow-hidden mt-6" ref={emblaRef}>
   <div className="flex gap-4 sm:gap-6">
     {partnerIcons.concat(partnerIcons).map((item, index) => (
@@ -873,6 +856,23 @@ const partnerIcons = [
   </div>
 </div>
 
+
+          <div className="flex flex-col items-center gap-7">
+            <div className="flex flex-col items-start w-[680px]">
+              <div className="flex flex-col items-start gap-1.5 w-full">
+                <div className="flex h-16 max-h-16 px-7 py-4.5 items-center w-full rounded-full bg-gray-200">
+                  <div className="flex-1 text-lg font-pretendard text-gray-500">검색어를 입력하세요.</div>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M10 4C6.68629 4 4 6.68629 4 10C4 13.3137 6.68629 16 10 16C13.3137 16 16 13.3137 16 10C16 6.68629 13.3137 4 10 4ZM2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 11.8487 17.3729 13.5509 16.3199 14.9056L21.7071 20.2929C22.0976 20.6834 22.0976 21.3166 21.7071 21.7071C21.3166 22.0976 20.6834 22.0976 20.2929 21.7071L14.9057 16.3198C13.551 17.3729 11.8487 18 10 18C5.58172 18 2 14.4183 2 10Z"
+                      fill="#616161"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
 
             <div className="flex justify-center items-center gap-5 w-full">
               <div className="text-base font-medium font-pretendard text-gray-900">#시행 · 시공</div>
