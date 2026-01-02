8cbe22cef5933115e8d1b2c5305ad452eb578516
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-02 11:55:34 +0100

    auto-commit

--- src/pages/NinethPage.jsx
+++ src/pages/NinethPage.jsx
@@ -26,7 +26,7 @@ export default function InquiryModal() {
   };
 
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center p-4 z-50">
+    <div className=" bg-black/40 flex items-center justify-center p-4 z-50">
       <div className="w-full max-w-[720px] bg-white rounded-[28px] shadow-2xl p-8 sm:p-10 relative">
 
         {/* Close button */}
