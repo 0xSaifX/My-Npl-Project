723bda52b59b5634b4ca0fdf67a7d0800ee859d1
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-02 10:47:13 +0100

    auto-commit

--- src/pages/EightPage.jsx
+++ src/pages/EightPage.jsx
@@ -40,8 +40,8 @@ export default function EightPage() {
 
          <div className="flex justify-center gap-8 mb-2 text-sm text-black font-bold flex-wrap">
             <a href="#/">투자대출 </a>
-            <span>공지사항</span>
-            <span>경매부동산</span>
+            <a href="#">공지사항</a>
+            <a href="#">경매부동산</a>
           </div>
         <div className="border-t border-gray-400 mb-6" />
       <div className="max-w-4xl mx-auto">
