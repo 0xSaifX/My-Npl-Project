753d5f6f06ff64817dc622c1b547a09edb9559f3
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-02 10:03:10 +0100

    auto-commit

--- src/pages/EightPage.jsx
+++ src/pages/EightPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function EightPage () {
   return (
     <div>
-      <h1 className='text-center align-middle'>
+      <h1 className='text-center align-middle justify-center'>
         Hello From the Eight Page
       </h1>
     </div>
