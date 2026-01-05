afa6de7a02692e181b96380ef971c81ee0840edf
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-05 09:55:40 +0100

    auto-commit

--- src/pages/SecondPage.jsx
+++ src/pages/SecondPage.jsx
@@ -143,36 +143,6 @@ export default function SecondPage() {
     </div>
       </section>
 
-
-      {/* PROJECTS
-      <section className="max-w-[1280px] mt-28 mx-20">
-        <h2 className="text-xl sm:text-2xl lg:text-3xl mb-5 font-bold">전국 모든 곳의 시행·시공 노하우</h2>
-        <p className="text-sm text-gray-500 mb-8">
-          지역별 프로젝트 사례를 확인해보세요.
-        </p>
-
-        <div className="flex gap-2 mb-10">
-          {["서울/수도권", "대전", "전주", "광주", "대구", "부산"].map((c) => (
-            <button
-              key={c}
-              className="px-4 py-2 rounded-full border text-sm hover:bg-black hover:text-white"
-            >
-              {c}
-            </button>
-          ))}
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <div key={i} className="flex flex-col gap-3">
-              <div className="h-[180px] rounded-xl bg-gray-200" />
-              <div className="font-medium">서울 빌딩</div>
-              <div className="text-xs text-gray-400">준공일 2025년 2월</div>
-            </div>
-          ))}
-        </div>
-      </section> */}
-
       {/* PROJECTS */}
       <section className="max-w-[1280px] mx-20 px-6 mt-32">
           <h2 className="text-xl sm:text-2xl lg:text-3xl mb-2 font-bold">전국 모든 곳의 시행·시공 노하우</h2>
