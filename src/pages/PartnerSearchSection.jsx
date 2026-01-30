f6bae1efa93c5b5fa9aa15d1390c3b71dc1e5aea
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-30 14:46:45 +0100

    auto-commit

---
+++ src/pages/PartnerSearchSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import amazon from "../assets/amazon.png";
+import lightcoin from "../assets/lightcoin.png";
+import mastercard from "../assets/mastercard.png";
+import skrill from "../assets/skrill.png";
+import visa from "../assets/visa.png";
+
