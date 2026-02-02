8369899dfa5667302260827dfe7cf56449175cac
0xSaifX <saifuddeen.tech@gmail.com>
2026-02-02 09:48:34 +0100

    auto-commit

--- src/pages/NinethPage.jsx
+++ src/pages/NinethPage.jsx
@@ -150,7 +150,7 @@ export default function InquiryModal() {
         {/* Submit */}
         <div className="flex justify-center">
           <button className="w-[220px] h-14 rounded-full bg-black text-white text-sm font-medium"
-          onClick={() => navigate(-1)}>
+          onClick={() => navigate(1)}>
             신청하기
           </button>
         </div>
